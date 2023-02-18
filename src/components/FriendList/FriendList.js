38c6faae7f126bd652f187d66f68544512389df6
nastiaknik <anastazja.knihnitska@gmail.com>
2023-02-18 15:54:13 +0100

    add component FriendListItem

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,28 +1,20 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 
 export const FriendList = ({ friends }) => {
   return (
     <ul className={css.friendList}>
-      {friends.map(friend => (
-        <li key={friend.id} className={css.item}>
-          <span
-            className={css.status}
-            style={
-              friend.isOnline
-                ? { backgroundColor: 'seagreen' }
-                : { backgroundColor: 'tomato' }
-            }
-          ></span>
-          <img
-            src={friend.avatar}
-            alt="User avatar"
-            width="48"
-            className={css.avatar}
+      {friends.map(friend => {
+        return (
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+            key={friend.id}
           />
-          <p className={css.name}>{friend.name}</p>
-        </li>
-      ))}
+        );
+      })}
     </ul>
   );
 };
@@ -33,7 +25,7 @@ FriendList.propTypes = {
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired,
+      /* id: PropTypes.number.isRequired, */
     }).isRequired
   ).isRequired,
 };
